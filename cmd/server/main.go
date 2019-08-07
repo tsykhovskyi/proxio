d19cf41a1b94da4831c24282f40166c86e9669bb
Igor Tsykhovskyi <igorppok@gmail.com>
2019-08-07 21:14:42 +0300

    structuring server cmd

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,68 +1,54 @@
 package main
 
 import (
-	"bufio"
+	"flag"
 	"fmt"
-	"github.com/gliderlabs/ssh"
-	gossh "golang.org/x/crypto/ssh"
-	"io"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
-	"log"
+	"os"
+	"proxio/server"
 )
 
-func publicKeyFile(file string) gossh.Signer {
-	buffer, err := ioutil.ReadFile(file)
-	if err != nil {
-		log.Fatalln(fmt.Sprintf("Cannot read SSH private key file %s", file))
-		return nil
+type ServerConfig struct {
+	Host           string `yaml:"host"`
+	SshPort        string `yaml:"ssh_port"`
+	PrivateKeyPath string `yaml:"private_key_path"`
+}
+
+var (
+	help           = flag.Bool("h", false, "print help")
+	configFilePath = flag.String("config", "./config.yaml", "path to config.yaml file")
+	config         *ServerConfig
+)
+
+func init() {
+	flag.Parse()
+	if *help {
+		helpText()
+		os.Exit(0)
 	}
 
-	key, err := gossh.ParsePrivateKey(buffer)
-	if err != nil {
-		log.Fatalln(fmt.Sprintf("Cannot parse SSH public key file %s", file))
-		return nil
+	data, err := ioutil.ReadFile(*configFilePath)
+	if nil != err {
+		panic("no configuration file")
 	}
+	config = &ServerConfig{
+		SshPort: "22",
+	}
+	err = yaml.Unmarshal(data, &config)
+	if nil != err {
+		panic("wrong configuration. " + err.Error())
+	}
+}
 
-	return key
+func helpText() {
+	fmt.Println("USAGE")
+	fmt.Println("  server [flags]")
+	fmt.Println("")
+	fmt.Println("FLAGS")
+	flag.PrintDefaults()
 }
 
 func main() {
-	handler := func(s ssh.Session) {
-		io.WriteString(s, "Hello world\n")
-
-		scanner := bufio.NewScanner(s)
-		for scanner.Scan() {
-			fmt.Fprintln(s, "got:", scanner.Text())
-		}
-
-	}
-
-	s := &ssh.Server{
-		Addr:    ":2222",
-		Handler: handler,
-		LocalPortForwardingCallback: func(ctx ssh.Context, destinationHost string, destinationPort uint32) bool {
-			return true
-		},
-		SessionRequestCallback: func(sess ssh.Session, requestType string) bool {
-			return true
-		},
-		ReversePortForwardingCallback: func(ctx ssh.Context, bindHost string, bindPort uint32) bool {
-			return true
-		},
-		PtyCallback: func(ctx ssh.Context, pty ssh.Pty) bool {
-			return false
-		},
-	}
-	s.AddHostKey(publicKeyFile("cmd/server/keys/id_rsa"))
-
-	tcpIpForwardHandler := &ssh.ForwardedTCPHandler{}
-	s.RequestHandlers = map[string]ssh.RequestHandler{
-		"tcpip-forward":        tcpIpForwardHandler.HandleSSHRequest,
-		"cancel-tcpip-forward": tcpIpForwardHandler.HandleSSHRequest,
-	}
-
-	s.ChannelHandlers = ssh.DefaultChannelHandlers
-	s.ChannelHandlers["direct-tcpip"] = ssh.DirectTCPIPHandler
-
-	log.Fatal(s.ListenAndServe())
+	server.StartServer(config.SshPort, config.PrivateKeyPath)
 }
---
+++ server/server.go
@@ -0,0 +1,72 @@
+package server
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/gliderlabs/ssh"
+	gossh "golang.org/x/crypto/ssh"
+	"io"
+	"io/ioutil"
+	"log"
+)
+
+func StartServer(port string, serverKeyPath string) {
+	handler := func(s ssh.Session) {
+		key := gossh.MarshalAuthorizedKey(s.PublicKey())
+		out := fmt.Sprintf("Hi, %s\n", key)
+		io.WriteString(s, out)
+
+		scanner := bufio.NewScanner(s)
+		for scanner.Scan() {
+			// fmt.Fprintln(s, "got:", scanner.Text())
+		}
+	}
+
+	s := &ssh.Server{
+		Addr:    ":" + port,
+		Handler: handler,
+		LocalPortForwardingCallback: func(ctx ssh.Context, destinationHost string, destinationPort uint32) bool {
+			return true
+		},
+		SessionRequestCallback: func(sess ssh.Session, requestType string) bool {
+			return true
+		},
+		ReversePortForwardingCallback: func(ctx ssh.Context, bindHost string, bindPort uint32) bool {
+			return true
+		},
+		PtyCallback: func(ctx ssh.Context, pty ssh.Pty) bool {
+			return false
+		},
+		PublicKeyHandler: func(ctx ssh.Context, key ssh.PublicKey) bool {
+			return true
+		},
+	}
+	s.AddHostKey(publicKeyFile(serverKeyPath))
+
+	tcpIpForwardHandler := &ssh.ForwardedTCPHandler{}
+	s.RequestHandlers = map[string]ssh.RequestHandler{
+		"tcpip-forward":        tcpIpForwardHandler.HandleSSHRequest,
+		"cancel-tcpip-forward": tcpIpForwardHandler.HandleSSHRequest,
+	}
+
+	s.ChannelHandlers = ssh.DefaultChannelHandlers
+	s.ChannelHandlers["direct-tcpip"] = ssh.DirectTCPIPHandler
+
+	log.Fatal(s.ListenAndServe())
+}
+
+func publicKeyFile(file string) gossh.Signer {
+	buffer, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatalln(fmt.Sprintf("Cannot read SSH private key file %s", file))
+		return nil
+	}
+
+	key, err := gossh.ParsePrivateKey(buffer)
+	if err != nil {
+		log.Fatalln(fmt.Sprintf("Cannot parse SSH public key file %s", file))
+		return nil
+	}
+
+	return key
+}
