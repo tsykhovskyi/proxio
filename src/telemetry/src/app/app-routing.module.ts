a44b27045a7ea6e1cf3f8218e836d162f2cf8d9e
Igor Tsykhovskyi <igorppok@gmail.com>
2020-05-02 18:56:53 +0300

    telemetry page

--- src/telemetry/src/app/app-routing.module.ts
+++ src/telemetry/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TelemetryComponent} from "./page/telemetry/telemetry.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TelemetryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/telemetry/src/app/app.module.ts
+++ src/telemetry/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TelemetryComponent } from './page/telemetry/telemetry.component';
+import { MatGridListModule } from "@angular/material/grid-list";
+import {MatSliderModule} from "@angular/material/slider";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelemetryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
