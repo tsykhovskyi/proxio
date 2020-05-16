7c8ee2dd187e5feba9512d4d5dd3ad04732e946a
Igor Tsykhovskyi <igorppok@gmail.com>
2020-05-16 23:15:54 +0300

    storage message update handling

--- src/proxio/ui/storage.go
+++ src/proxio/ui/storage.go
@@ -4,26 +4,26 @@ import "proxio/client"
 
 func NewStorage() *Storage {
 	return &Storage{
-		messages: make(map[int]*client.Message),
-		domains:  make(map[string][]*client.Message),
+		domains: make(map[string][]*client.Message),
 	}
 }
 
 type Storage struct {
-	messages map[int]*client.Message
-	domains  map[string][]*client.Message
+	domains map[string][]*client.Message
 }
 
 func (s *Storage) Add(m *client.Message) {
-	s.messages[m.Id] = m
-
 	messages, ok := s.domains[m.Request.Host]
 	if !ok {
 		s.domains[m.Request.Host] = make([]*client.Message, 0)
 		messages = s.domains[m.Request.Host]
 	}
 
-	s.domains[m.Request.Host] = append(messages, m)
+	if size := len(messages); size > 0 && messages[size-1].Id == m.Id {
+		messages[size-1] = m
+	} else {
+		s.domains[m.Request.Host] = append(messages, m)
+	}
 }
 
 func (s *Storage) All(domain string) []*client.Message {
